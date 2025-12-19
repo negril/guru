negril/guru

commit 2585c9f1adb31d0d09c9a29fa249194c68957617

*/*: leading whitespaces -> tab
command used:
```
find -type f -name '*.ebuild' -exec sed -i -E 's/^    /\t/g' {} +
```

I was running this command anyway after when I was working on a few
packages because my formatter broke, decided to run it on the root of
the repo then because I was already gonna run it on the packages that I
was working on

I checked each package manually and ran pkgdev to actually see if it was
emitting any warnings, and all of them did for the leading whitepsaces

Signed-off-by: ingenarel <ingenarelitems@gmail.com>

dev-cpp/robotraconteur-companion/robotraconteur-companion-0.4.2.ebuild
@@ -17,28 +17,28 @@ KEYWORDS="~amd64 ~arm ~arm64 ~x86"
 IUSE=""
 
 DEPEND="dev-libs/boost
-    dev-libs/openssl
-    dev-build/cmake
-    dev-cpp/yaml-cpp
-    dev-cpp/eigen
-    dev-cpp/robotraconteur
+	dev-libs/openssl
+	dev-build/cmake
+	dev-cpp/yaml-cpp
+	dev-cpp/eigen
+	dev-cpp/robotraconteur
 "
 RDEPEND="${DEPEND}"
 
 src_configure() {
 local mycmakeargs=(
-        -DCMAKE_SKIP_RPATH=ON
-        -DBUILD_TESTING=OFF
-        -DBUILD_DOCUMENTATION=OFF
-        -DROBOTRACONTEUR_COMPANION_SOVERSION_MAJOR_ONLY=ON
-    )
-    cmake_src_configure
+	    -DCMAKE_SKIP_RPATH=ON
+	    -DBUILD_TESTING=OFF
+	    -DBUILD_DOCUMENTATION=OFF
+	    -DROBOTRACONTEUR_COMPANION_SOVERSION_MAJOR_ONLY=ON
+	)
+	cmake_src_configure
 }
 
 src_compile() {
-    cmake_src_compile
+	cmake_src_compile
 }
 
 src_install() {
-    cmake_src_install
+	cmake_src_install
 }

commit a0a7f9f2df3374f95cd700daa2fcff7fc8092729

*/*: fix leading whitespaces. Fix obvious QA issues
Fixes: 2585c9f1adb31d0d09c9a29fa249194c68957617
Signed-off-by: Paul Zander <negril.nx+gentoo@gmail.com>

dev-cpp/robotraconteur-companion/robotraconteur-companion-0.4.2.ebuild
@@ -7,8 +7,10 @@ inherit cmake
 
 DESCRIPTION="Robot Raconteur C++ Companion Library"
 HOMEPAGE="https://github.com/robotraconteur/robotraconteur_companion"
-SRC_URI="https://github.com/robotraconteur/robotraconteur_companion/releases/download/v${PV}/RobotRaconteurCompanion-${PV}-Source.tar.gz"
 
+SRC_URI="
+	https://github.com/robotraconteur/robotraconteur_companion/releases/download/v${PV}/RobotRaconteurCompanion-${PV}-Source.tar.gz
+"
 S="${WORKDIR}/RobotRaconteurCompanion-${PV}-Source"
 
 LICENSE="Apache-2.0"
@@ -24,11 +26,11 @@ DEPEND="dev-libs/boost:=
 RDEPEND="${DEPEND}"
 
 src_configure() {
-local mycmakeargs=(
-	    -DCMAKE_SKIP_RPATH=ON
-	    -DBUILD_TESTING=OFF
-	    -DBUILD_DOCUMENTATION=OFF
-	    -DROBOTRACONTEUR_COMPANION_SOVERSION_MAJOR_ONLY=ON
+	local mycmakeargs=(
+		-DCMAKE_SKIP_RPATH=ON
+		-DBUILD_TESTING=OFF
+		-DBUILD_DOCUMENTATION=OFF
+		-DROBOTRACONTEUR_COMPANION_SOVERSION_MAJOR_ONLY=ON
 	)
 	cmake_src_configure
 }

dev-cpp/robotraconteur/robotraconteur-1.2.7.ebuild
@@ -23,9 +23,11 @@ DEPEND="dev-libs/boost:=
 	dev-libs/libusb
 	sys-apps/dbus
 	net-wireless/bluez
-	python? ( dev-python/numpy[${PYTHON_USEDEP}]
-	          dev-python/setuptools[${PYTHON_USEDEP}]
-	          dev-python/pip[${PYTHON_USEDEP}] )
+	python? (
+		dev-python/numpy[${PYTHON_USEDEP}]
+		dev-python/setuptools[${PYTHON_USEDEP}]
+		dev-python/pip[${PYTHON_USEDEP}]
+	)
 "
 RDEPEND="
 	${DEPEND}
@@ -40,38 +42,38 @@ REQUIRED_USE="
 
 python_configure() {
 	local mycmakeargs=(
-	    -DCMAKE_SKIP_RPATH=ON
-	    -DBUILD_GEN=ON
-	    -DBUILD_TESTING=OFF
-	    -DBUILD_DOCUMENTATION=OFF
-	    -DBUILD_PYTHON3=ON
-	    -DINSTALL_PYTHON3_PIP=ON
-	    -DINSTALL_PYTHON3_PIP_EXTRA_ARGS="--compile --use-pep517 --no-build-isolation --no-deps --root-user-action=ignore"
-	    -DROBOTRACONTEURCORE_SOVERSION_MAJOR_ONLY=ON
+		-DCMAKE_SKIP_RPATH=ON
+		-DBUILD_GEN=ON
+		-DBUILD_TESTING=OFF
+		-DBUILD_DOCUMENTATION=OFF
+		-DBUILD_PYTHON3=ON
+		-DINSTALL_PYTHON3_PIP=ON
+		-DINSTALL_PYTHON3_PIP_EXTRA_ARGS="--compile --use-pep517 --no-build-isolation --no-deps --root-user-action=ignore"
+		-DROBOTRACONTEURCORE_SOVERSION_MAJOR_ONLY=ON
 	)
 	cmake_src_configure
 }
 
 src_configure() {
 	if use python; then
-	    python_foreach_impl python_configure
+		python_foreach_impl python_configure
 	else
-	    local mycmakeargs=(
-	        -DCMAKE_SKIP_RPATH=ON
-	        -DBUILD_GEN=ON
-	        -DBUILD_TESTING=OFF
-	        -DBUILD_DOCUMENTATION=OFF
-	        -DROBOTRACONTEURCORE_SOVERSION_MAJOR_ONLY=ON
-	    )
-	    cmake_src_configure
+		local mycmakeargs=(
+			-DCMAKE_SKIP_RPATH=ON
+			-DBUILD_GEN=ON
+			-DBUILD_TESTING=OFF
+			-DBUILD_DOCUMENTATION=OFF
+			-DROBOTRACONTEURCORE_SOVERSION_MAJOR_ONLY=ON
+		)
+		cmake_src_configure
 	fi
 }
 
 src_compile() {
 	if use python; then
-	    python_foreach_impl cmake_src_compile
+		python_foreach_impl cmake_src_compile
 	else
-	    cmake_src_compile
+		cmake_src_compile
 	fi
 }
 
@@ -82,8 +84,8 @@ python_install(){
 
 src_install() {
 	if use python; then
-	    python_foreach_impl python_install
+		python_foreach_impl python_install
 	else
-	    cmake_src_install
+		cmake_src_install
 	fi
 }

dev-python/cloudflare/cloudflare-4.2.0.ebuild
@@ -7,56 +7,60 @@ PYTHON_COMPAT=( python3_{11..13} )
 # upstream doesn't yet officially claim 3.13 support but passes tests and works
 DISTUTILS_USE_PEP517="hatchling"
 inherit distutils-r1
+
 DESCRIPTION="Python wrapper for the Cloudflare v4 API"
 HOMEPAGE="https://pypi.org/project/cloudflare/"
-if [ "${PV}" == 9999 ]; then
+
+if [[ "${PV}" == 9999 ]]; then
 	EGIT_REPO_URI="https://github.com/cloudflare/cloudflare-python"
 	inherit git-r3
 else
 	MYPV=$(ver_rs 3 -)
 	MYPV=${MYPV/beta/beta.}
 	MYPN="cloudflare-python"
-	SRC_URI="https://github.com/cloudflare/cloudflare-python/archive/refs/tags/v${MYPV}.tar.gz -> ${P}.gh.tar.gz
-		test? ( https://github.com/Phoenix591/${MYPN}/releases/download/${PV}/${MYPN}-${PV}-prism.tar.gz )"
-	#Prism archive generated by workflow
+	SRC_URI="
+		https://github.com/cloudflare/cloudflare-python/archive/refs/tags/v${MYPV}.tar.gz -> ${P}.gh.tar.gz
+		test? (
+			https://github.com/Phoenix591/${MYPN}/releases/download/${PV}/${MYPN}-${PV}-prism.tar.gz
+		)
+	"
+	# Prism archive generated by workflow
 	# https://github.com/Phoenix591/cloudflare-python/blob/master/.github/workflows/test-tar.yml
 	S="${WORKDIR}/${MYPN}-${MYPV}"
 	KEYWORDS="~amd64 ~arm64"
 fi
+
 LICENSE="MIT test? ( ISC Apache-2.0 MIT BSD CC0-1.0 0BSD )"
 # nodejs module and deps used to test
 SLOT="0"
-RDEPEND=" ${DEPEND}
+
+RDEPEND="${DEPEND}
 	>=dev-python/httpx-0.23.0[${PYTHON_USEDEP}]
 	>=dev-python/pydantic-2.9.2[${PYTHON_USEDEP}]
 	>=dev-python/typing-extensions-4.7.0[${PYTHON_USEDEP}]
 	>=dev-python/anyio-4.3.0[${PYTHON_USEDEP}]
 	>=dev-python/distro-1.7.0[${PYTHON_USEDEP}]
 	>=dev-python/sniffio-1.3.1[${PYTHON_USEDEP}]
-	 "
+"
 
-BDEPEND="test? (
-	>=net-libs/nodejs-18.20.1
-	 dev-python/pytest-asyncio[${PYTHON_USEDEP}]
-	 dev-python/time-machine[${PYTHON_USEDEP}]
-	 dev-python/dirty-equals[${PYTHON_USEDEP}]
-	 dev-python/respx[${PYTHON_USEDEP}]
-	${RDEPEND}
-)"
+BDEPEND="
+	test? (
+		>=net-libs/nodejs-18.20.1
+		dev-python/pytest-asyncio[${PYTHON_USEDEP}]
+		dev-python/time-machine[${PYTHON_USEDEP}]
+		dev-python/dirty-equals[${PYTHON_USEDEP}]
+		dev-python/respx[${PYTHON_USEDEP}]
+		${RDEPEND}
+	)
+"
 
 distutils_enable_tests pytest
-RESTRICT+=" !test? ( test )"
-
-src_unpack() {
-	unpack "${P}.gh.tar.gz"
-	use test && cd "${S}" && unpack "cloudflare-python-${PV}-prism.tar.gz"
-}
 
 #python_prepare_all() {
 #	# don't install tests or examples
 #	sed -i -e "s/'cli4', 'examples'/'cli4'/" \
 #		-e "s#'CloudFlare/tests',##" \
-#		 setup.py || die
+#		setup.py || die
 #	sed -i -e "/def test_ips7_should_fail():/i@pytest.mark.xfail(reason='Now fails upstream')" \
 #		-e "2s/^/import pytest/" \
 #		CloudFlare/tests/test_cloudflare_calls.py || die
@@ -67,37 +71,41 @@ python_test() {
 	# these 2 tests fail in an ebuild environment for some reason
 	# help appreciated
 	local EPYTEST_DESELECT=(
-	tests/test_client.py::TestCloudflare::test_validate_headers
-	tests/test_client.py::TestAsyncCloudflare::test_validate_headers )
-		#intermittently fail for unknown reasons, passed along to upstream
-		EPYTEST_DESELECT+=(
+		tests/test_client.py::TestCloudflare::test_validate_headers
+		tests/test_client.py::TestAsyncCloudflare::test_validate_headers
+	)
+	# intermittently fail for unknown reasons, passed along to upstream
+	EPYTEST_DESELECT+=(
 		tests/test_client.py::TestAsyncCloudflare::test_copy_build_request
 		tests/test_client.py::TestCloudflare::test_copy_build_request
 		tests/test_client.py::TestAsyncCloudflare::test_get_platform
-)
+	)
+
 	epytest
 }
 
-src_test() {
-	start_mock
-	distutils-r1_src_test
-	stop_mock
-}
 start_mock() {
-# Run prism mock api server, this is what needs nodejs
+	# Run prism mock api server, this is what needs nodejs
 	node --no-warnings node_modules/@stainless-api/prism-cli/dist/index.js mock \
 		"cloudflare-spec.yml" >prism.log || die "Failed starting prism" &
 	echo $! >"${T}/mock.pid" || die
 	# Wait for server to come online
 	echo -n "Waiting for mockserver"
 	while ! grep -q "✖  fatal\|Prism is listening" "prism.log" ; do
-	    echo -n "." || die
-	    sleep 0.5
+		echo -n "." || die
+		sleep 0.5 || die
 	done
 	if grep -q "✖  fatal" prism.log; then
 		die "Prism mock server failed"
 	fi
 }
+
 stop_mock() {
 	kill $(cat "${T}/mock.pid") || die
+}
+
+src_test() {
+	start_mock
+	distutils-r1_src_test
+	stop_mock
 }

dev-python/cloudflare/cloudflare-4.3.1.ebuild
@@ -7,57 +7,61 @@ PYTHON_COMPAT=( python3_{11..13} )
 # upstream doesn't yet officially claim 3.13 support but passes tests and works
 DISTUTILS_USE_PEP517="hatchling"
 inherit distutils-r1
+
 DESCRIPTION="Python wrapper for the Cloudflare v4 API"
 HOMEPAGE="https://pypi.org/project/cloudflare/"
-if [ "${PV}" == 9999 ]; then
+
+if [[ "${PV}" == 9999 ]]; then
 	EGIT_REPO_URI="https://github.com/cloudflare/cloudflare-python"
 	inherit git-r3
 else
 	MYPV=$(ver_rs 3 -)
 	MYPV=${MYPV/beta/beta.}
 	MYPN="cloudflare-python"
-	SRC_URI="https://github.com/cloudflare/cloudflare-python/archive/refs/tags/v${MYPV}.tar.gz -> ${P}.gh.tar.gz
-		test? ( https://github.com/Phoenix591/${MYPN}/releases/download/${PV}/${MYPN}-${PV}-prism.tar.gz )"
-	#Prism archive generated by workflow
+	SRC_URI="
+		https://github.com/cloudflare/cloudflare-python/archive/refs/tags/v${MYPV}.tar.gz -> ${P}.gh.tar.gz
+		test? (
+			https://github.com/Phoenix591/${MYPN}/releases/download/${PV}/${MYPN}-${PV}-prism.tar.gz
+		)
+	"
+	# Prism archive generated by workflow
 	# https://github.com/Phoenix591/cloudflare-python/blob/master/.github/workflows/test-tar.yml
 	S="${WORKDIR}/${MYPN}-${MYPV}"
 	KEYWORDS="~amd64 ~arm64"
 fi
+
 LICENSE="MIT test? ( ISC Apache-2.0 MIT BSD CC0-1.0 0BSD )"
 # nodejs module and deps used to test
 SLOT="0"
-RDEPEND=" ${DEPEND}
+
+RDEPEND="${DEPEND}
 	>=dev-python/httpx-0.23.0[${PYTHON_USEDEP}]
 	>=dev-python/pydantic-2.9.2[${PYTHON_USEDEP}]
 	>=dev-python/typing-extensions-4.7.0[${PYTHON_USEDEP}]
 	>=dev-python/anyio-4.3.0[${PYTHON_USEDEP}]
 	>=dev-python/distro-1.7.0[${PYTHON_USEDEP}]
 	>=dev-python/sniffio-1.3.1[${PYTHON_USEDEP}]
-	 "
+"
 
-BDEPEND="test? (
-	>=net-libs/nodejs-18.20.1
-	 dev-python/pytest-asyncio[${PYTHON_USEDEP}]
-	 dev-python/time-machine[${PYTHON_USEDEP}]
-	 dev-python/dirty-equals[${PYTHON_USEDEP}]
-	 dev-python/respx[${PYTHON_USEDEP}]
-	${RDEPEND}
-)"
+BDEPEND="
+	test? (
+		>=net-libs/nodejs-18.20.1
+		dev-python/pytest-asyncio[${PYTHON_USEDEP}]
+		dev-python/time-machine[${PYTHON_USEDEP}]
+		dev-python/dirty-equals[${PYTHON_USEDEP}]
+		dev-python/respx[${PYTHON_USEDEP}]
+		${RDEPEND}
+	)
+"
 
 EPYTEST_XDIST=1
 distutils_enable_tests pytest
-RESTRICT+=" !test? ( test )"
-
-src_unpack() {
-	unpack "${P}.gh.tar.gz"
-	use test && cd "${S}" && unpack "cloudflare-python-${PV}-prism.tar.gz"
-}
 
 #python_prepare_all() {
 #	# don't install tests or examples
 #	sed -i -e "s/'cli4', 'examples'/'cli4'/" \
 #		-e "s#'CloudFlare/tests',##" \
-#		 setup.py || die
+#		setup.py || die
 #	sed -i -e "/def test_ips7_should_fail():/i@pytest.mark.xfail(reason='Now fails upstream')" \
 #		-e "2s/^/import pytest/" \
 #		CloudFlare/tests/test_cloudflare_calls.py || die
@@ -68,42 +72,46 @@ python_test() {
 	# these 2 tests fail in an ebuild environment for some reason
 	# help appreciated
 	local EPYTEST_DESELECT=(
-	tests/test_client.py::TestCloudflare::test_validate_headers
-	tests/test_client.py::TestAsyncCloudflare::test_validate_headers )
-		#intermittently fail for unknown reasons, passed along to upstream
-		EPYTEST_DESELECT+=(
+		tests/test_client.py::TestCloudflare::test_validate_headers
+		tests/test_client.py::TestAsyncCloudflare::test_validate_headers
+	)
+	# intermittently fail for unknown reasons, passed along to upstream
+	EPYTEST_DESELECT+=(
 		tests/test_client.py::TestAsyncCloudflare::test_copy_build_request
 		tests/test_client.py::TestCloudflare::test_copy_build_request
 		tests/test_client.py::TestAsyncCloudflare::test_get_platform
-)
+	)
 	# Doesn't have authentication setup correctly, broken upstream too
 	local EPYTEST_IGNORE=(
 		tests/api_resources/cloudforce_one/threat_events/datasets/test_health.py
 		tests/api_resources/radar/
-)
+	)
+
 	epytest
 }
 
-src_test() {
-	start_mock
-	distutils-r1_src_test
-	stop_mock
-}
 start_mock() {
-# Run prism mock api server, this is what needs nodejs
+	# Run prism mock api server, this is what needs nodejs
 	node --no-warnings node_modules/@stainless-api/prism-cli/dist/index.js mock \
 		"cloudflare-spec.yml" >prism.log || die "Failed starting prism" &
 	echo $! >"${T}/mock.pid" || die
 	# Wait for server to come online
 	echo -n "Waiting for mockserver"
 	while ! grep -q "✖  fatal\|Prism is listening" "prism.log" ; do
-	    echo -n "." || die
-	    sleep 0.5
+		echo -n "." || die
+		sleep 0.5 || die
 	done
 	if grep -q "✖  fatal" prism.log; then
 		die "Prism mock server failed"
 	fi
 }
+
 stop_mock() {
 	kill $(cat "${T}/mock.pid") || die
+}
+
+src_test() {
+	start_mock
+	distutils-r1_src_test
+	stop_mock
 }

dev-python/osc-placement/osc-placement-4.7.0.ebuild
@@ -26,7 +26,7 @@ RDEPEND="
 "
 BDEPEND="
 	test? (
-	    >=dev-python/coverage-4.0.0[${PYTHON_USEDEP}]
+		>=dev-python/coverage-4.0.0[${PYTHON_USEDEP}]
 		>=dev-python/oslotest-1.10.0[${PYTHON_USEDEP}]
 		>=dev-python/python-openstackclient-3.3.0[${PYTHON_USEDEP}]
 		>=dev-python/stestr-1.0.0[${PYTHON_USEDEP}]

gui-apps/awww/awww-9999.ebuild
@@ -4,23 +4,23 @@
 # Autogenerated by pycargoebuild 0.15.0
 
 EAPI=8
+RUST_MIN_VER="1.89.0"
 
 inherit cargo shell-completion
 
 DESCRIPTION="Efficient animated wallpaper daemon for wayland, controlled at runtime"
 HOMEPAGE="https://codeberg.org/LGFae/awww"
+
 if [[ ${PV} == *9999* ]]; then
 	inherit git-r3
 	EGIT_REPO_URI="https://codeberg.org/LGFae/${PN}.git"
 else
 	SRC_URI="
-	https://codeberg.org/LGFae/${PN}/archive/refs/tags/v${PV}.tar.gz -> ${P}.tar.gz
-	${CARGO_CRATE_URIS}
+		https://codeberg.org/LGFae/${PN}/archive/refs/tags/v${PV}.tar.gz -> ${P}.tar.gz
+		${CARGO_CRATE_URIS}
 	"
 	KEYWORDS="~amd64"
 fi
-
-S="${WORKDIR}/${PN}"
 
 LICENSE="GPL-3"
 # Dependent crate licenses
@@ -29,7 +29,6 @@ LICENSE+="
 	ZLIB
 "
 SLOT="0"
-RUST_MIN_VER="1.89.0"
 
 DEPEND="
 	app-arch/lz4:=
@@ -47,10 +46,10 @@ QA_FLAGS_IGNORED="
 
 src_unpack() {
 	if [[ "${PV}" == *9999* ]]; then
-	    git-r3_src_unpack
-	    cargo_live_src_unpack
+		git-r3_src_unpack
+		cargo_live_src_unpack
 	else
-	    cargo_src_unpack
+		cargo_src_unpack
 	fi
 }
 

gui-apps/awww/metadata.xml
@@ -2,8 +2,8 @@
 <!DOCTYPE pkgmetadata SYSTEM "https://www.gentoo.org/dtd/metadata.dtd">
 <pkgmetadata>
 	 <!-- maintainer-needed -->
-    <upstream>
-        <remote-id type="codeberg">LGFae/awww</remote-id>
-        <bugs-to>https://codeberg.org/LGFae/awww/issues</bugs-to>
-    </upstream>
+	<upstream>
+		<remote-id type="codeberg">LGFae/awww</remote-id>
+		<bugs-to>https://codeberg.org/LGFae/awww/issues</bugs-to>
+	</upstream>
 </pkgmetadata>

gui-apps/swaylock-plugin/swaylock-plugin-1.8.0.ebuild
@@ -5,10 +5,9 @@ EAPI=8
 
 inherit fcaps meson
 
-if [[ "${PV}" == 9999 ]]
-then
-	  inherit git-r3
-	  EGIT_REPO_URI="https://github.com/mstoeckl/${PN}.git"
+if [[ "${PV}" == 9999 ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="https://github.com/mstoeckl/${PN}.git"
 else
 	SRC_URI="https://github.com/mstoeckl/swaylock-plugin/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 	KEYWORDS="~amd64"

gui-apps/swaylock-plugin/swaylock-plugin-9999.ebuild
@@ -5,10 +5,9 @@ EAPI=8
 
 inherit fcaps meson
 
-if [[ "${PV}" == 9999 ]]
-then
-	  inherit git-r3
-	  EGIT_REPO_URI="https://github.com/mstoeckl/${PN}.git"
+if [[ "${PV}" == 9999 ]]; then
+	inherit git-r3
+	EGIT_REPO_URI="https://github.com/mstoeckl/${PN}.git"
 else
 	SRC_URI="https://github.com/mstoeckl/swaylock-plugin/archive/v${PV}.tar.gz -> ${P}.tar.gz"
 	KEYWORDS="~amd64"

gui-apps/windowtolayer/windowtolayer-9999.ebuild
@@ -40,8 +40,8 @@ DESCRIPTION="Display existing Wayland applications as a wallpaper"
 HOMEPAGE="https://gitlab.freedesktop.org/mstoeckl/windowtolayer"
 
 if [[ ${PV} == 9999 ]]; then
-	  inherit git-r3
-	  EGIT_REPO_URI="https://gitlab.freedesktop.org/mstoeckl/${PN}.git"
+	inherit git-r3
+	EGIT_REPO_URI="https://gitlab.freedesktop.org/mstoeckl/${PN}.git"
 else
 	SRC_URI="
 		https://gitlab.freedesktop.org/mstoeckl/windowtolayer/-/archive/v${PV}/${PN}-v${PV}.tar.bz2 -> ${P}.tar.bz2
@@ -61,8 +61,8 @@ QA_FLAGS_IGNORED="/usr/bin/${PN}"
 
 src_unpack() {
 	if [[ ${PV} == 9999 ]]; then
-		  git-r3_src_unpack
-		  cargo_live_src_unpack
+		git-r3_src_unpack
+		cargo_live_src_unpack
 	else
 		cargo_src_unpack
 	fi

gui-wm/hyprland-contrib/hyprland-contrib-0.1-r1.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -53,30 +53,30 @@ BDEPEND="
 DEPEND="${RDEPEND}"
 
 src_install() {
-	if use grimblast; then
-	   pushd grimblast || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		if use grimblast; then
+		pushd grimblast || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use hyprprop; then
-	   pushd hyprprop || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd hyprprop || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use scratchpad; then
-	   pushd scratchpad || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd scratchpad || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use shellevents; then
-	   pushd shellevents || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd shellevents || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use swap; then
-	   pushd try_swap_workspace || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd try_swap_workspace || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 }
 

gui-wm/hyprland-contrib/hyprland-contrib-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 1999-2023 Gentoo Authors
+# Copyright 1999-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -7,6 +7,7 @@ inherit optfeature
 
 DESCRIPTION="Community-maintained extensions for hyprland"
 HOMEPAGE="https://hyprland.org/"
+
 if [[ ${PV} == 9999 ]]; then
 	EGIT_REPO_URI="https://github.com/hyprwm/contrib.git"
 	inherit git-r3
@@ -53,35 +54,35 @@ DEPEND="${RDEPEND}"
 
 src_install() {
 	if use grimblast; then
-	   pushd grimblast || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd grimblast || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use hyprprop; then
-	   pushd hyprprop || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd hyprprop || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use hdrop; then
-	   pushd hdrop || die
-	   PREFIX="${D}/usr" emake hdrop.1 #PR:80 will fix this
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd hdrop || die
+		PREFIX="${D}/usr" emake hdrop.1 #PR:80 will fix this
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use scratchpad; then
-	   pushd scratchpad || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd scratchpad || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use shellevents; then
-	   pushd shellevents || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd shellevents || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 	if use swap; then
-	   pushd try_swap_workspace || die
-	   PREFIX="${D}/usr" emake install
-	   popd || die
+		pushd try_swap_workspace || die
+		PREFIX="${D}/usr" emake install
+		popd || die
 	fi
 }
 

media-sound/eartag/eartag-0.5.0.ebuild
@@ -20,26 +20,26 @@ RESTRICT="!test? ( test )"
 REQUIRED_USE="${PYTHON_REQUIRED_USE}"
 
 DEPEND="
-		${PYTHON_DEPS}
-		>=gui-libs/gtk-4.12.0
-		>=gui-libs/libadwaita-1.4.0
-		>=dev-python/pygobject-3.44.1
-		>=dev-python/pillow-10.0.0
-		>=media-libs/mutagen-1.46.0
-		>=dev-python/python-magic-0.4.27
-		>=dev-python/pyacoustid-1.2.2-r1
-		test? ( >=dev-python/pytest-7.4.0 )
+	${PYTHON_DEPS}
+	>=gui-libs/gtk-4.12.0
+	>=gui-libs/libadwaita-1.4.0
+	>=dev-python/pygobject-3.44.1
+	>=dev-python/pillow-10.0.0
+	>=media-libs/mutagen-1.46.0
+	>=dev-python/python-magic-0.4.27
+	>=dev-python/pyacoustid-1.2.2-r1
+	test? ( >=dev-python/pytest-7.4.0 )
 "
 RDEPEND="${DEPEND}"
 BDEPEND="
-		>=dev-build/meson-1.2.1-r1
+	>=dev-build/meson-1.2.1-r1
 "
 
 src_install() {
 	sed -i \
 		-e 's/^#!.*/#!\/usr\/bin\/python3/g' \
 		"${S}-build/src/eartag" \
-	    || die
+	|| die
 
 	meson_src_install
 	python_optimize

media-sound/eartag/eartag-0.5.1.ebuild
@@ -20,26 +20,26 @@ RESTRICT="!test? ( test )"
 REQUIRED_USE="${PYTHON_REQUIRED_USE}"
 
 DEPEND="
-		${PYTHON_DEPS}
-		>=gui-libs/gtk-4.12.0
-		>=gui-libs/libadwaita-1.4.0
-		>=dev-python/pygobject-3.44.1
-		>=dev-python/pillow-10.0.0
-		>=media-libs/mutagen-1.46.0
-		>=dev-python/python-magic-0.4.27
-		>=dev-python/pyacoustid-1.2.2-r1
-		test? ( >=dev-python/pytest-7.4.0 )
+	${PYTHON_DEPS}
+	>=gui-libs/gtk-4.12.0
+	>=gui-libs/libadwaita-1.4.0
+	>=dev-python/pygobject-3.44.1
+	>=dev-python/pillow-10.0.0
+	>=media-libs/mutagen-1.46.0
+	>=dev-python/python-magic-0.4.27
+	>=dev-python/pyacoustid-1.2.2-r1
+	test? ( >=dev-python/pytest-7.4.0 )
 "
 RDEPEND="${DEPEND}"
 BDEPEND="
-		>=dev-build/meson-1.2.1-r1
+	>=dev-build/meson-1.2.1-r1
 "
 
 src_install() {
 	sed -i \
 		-e 's/^#!.*/#!\/usr\/bin\/python3/g' \
 		"${S}-build/src/eartag" \
-	    || die
+	|| die
 
 	meson_src_install
 	python_optimize

media-sound/goxlr-utility/goxlr-utility-1.2.3.ebuild
@@ -655,7 +655,7 @@ CRATES="
 	zvariant_utils@3.2.1
 "
 
-declare -A GIT_CRATES=(
+declare -gA GIT_CRATES=(
 	[xpc-connection-sys]='https://github.com/dfrankland/xpc-connection-rs;cd4fb3d05edb4292ccb9566ae27cdeb874222d2a;xpc-connection-rs-%commit%/xpc-connection-sys'
 )
 

media-sound/goxlr-utility/goxlr-utility-9999.ebuild
@@ -21,9 +21,9 @@ fi
 
 LICENSE="MIT Music-Tribe"
 LICENSE+="
-	    0BSD Apache-2.0 Apache-2.0-with-LLVM-exceptions BSD-2 BSD CC0-1.0
-	    CDLA-Permissive-2.0 ISC MIT MPL-2.0 UoI-NCSA Unicode-3.0 Unlicense
-	    ZLIB
+	0BSD Apache-2.0 Apache-2.0-with-LLVM-exceptions BSD-2 BSD CC0-1.0
+	CDLA-Permissive-2.0 ISC MIT MPL-2.0 UoI-NCSA Unicode-3.0 Unlicense
+	ZLIB
 "
 SLOT="0"
 IUSE="tts"

media-sound/spot/spot-0.4.0-r2.ebuild
@@ -390,24 +390,28 @@ IUSE="test"
 RESTRICT="test"
 
 DEPEND="
-		>=dev-libs/glib-2.76.3:2
-		>=dev-libs/openssl-3.0.10:=
-		>=gui-libs/gtk-4.10.4:4
-		>=gui-libs/libadwaita-1.3.3:1
-		>=media-libs/alsa-lib-1.2.9
-		>=media-libs/libpulse-15.0[glib]
-		virtual/zlib:=
-		x11-libs/cairo
-		x11-libs/gdk-pixbuf:2
-		x11-libs/pango
+	>=dev-libs/glib-2.76.3:2
+	>=dev-libs/openssl-3.0.10:=
+	>=gui-libs/gtk-4.10.4:4
+	>=gui-libs/libadwaita-1.3.3:1
+	>=media-libs/alsa-lib-1.2.9
+	>=media-libs/libpulse-15.0[glib]
+	virtual/zlib:=
+	x11-libs/cairo
+	x11-libs/gdk-pixbuf:2
+	x11-libs/pango
 "
 RDEPEND="${DEPEND}"
 BDEPEND="
-		app-alternatives/ninja
-		>=dev-build/meson-1.1.1
-		>=dev-libs/appstream-glib-0.8.2
-		test? ( || ( dev-lang/rust[clippy]
-					 dev-lang/rust-bin[clippy] ) )
+	app-alternatives/ninja
+	>=dev-build/meson-1.1.1
+	>=dev-libs/appstream-glib-0.8.2
+	test? (
+		|| (
+			dev-lang/rust[clippy]
+			dev-lang/rust-bin[clippy]
+		)
+	)
 "
 
 # rust does not use *FLAGS from make.conf, silence portage warning
@@ -429,8 +433,8 @@ src_configure() {
 		"${S}"/data/meson.build \
 		|| die
 	mv "${S}/data/dev.alextren.Spot.appdata.xml" \
-	   "${S}/data/dev.alextren.Spot.metainfo.xml" \
-	   || die
+		"${S}/data/dev.alextren.Spot.metainfo.xml" \
+		|| die
 
 	# empty cargo.sh, compiling is done by cargo_src_compile
 	echo "#!/bin/bash" > "${S}/build-aux/cargo.sh" \

net-im/mautrix-slack/mautrix-slack-0.1.3.ebuild
@@ -59,5 +59,5 @@ pkg_postinst() {
 
 pkg_config() {
 	su - "${PN}" -s /bin/sh -c \
-	   "/usr/bin/${PN} -c /etc/mautrix/${PN/-/_}.yaml -g -r /var/lib/${PN/-/\/}/registration.yaml"
+		"/usr/bin/${PN} -c /etc/mautrix/${PN/-/_}.yaml -g -r /var/lib/${PN/-/\/}/registration.yaml"
 }

net-im/mautrix-slack/mautrix-slack-0.1.4.ebuild
@@ -62,5 +62,5 @@ pkg_postinst() {
 
 pkg_config() {
 	su - "${PN}" -s /bin/sh -c \
-	   "/usr/bin/${PN} -c /etc/mautrix/${PN/-/_}.yaml -g -r /var/lib/${PN/-/\/}/registration.yaml"
+		"/usr/bin/${PN} -c /etc/mautrix/${PN/-/_}.yaml -g -r /var/lib/${PN/-/\/}/registration.yaml"
 }

net-libs/kcgi/kcgi-0.13.3.ebuild
@@ -1,9 +1,9 @@
-# Copyright 2019-2021 Gentoo Authors
+# Copyright 2019-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
 
-inherit flag-o-matic multilib multiprocessing toolchain-funcs
+inherit flag-o-matic toolchain-funcs
 
 DESCRIPTION="Minimal CGI library for web applications"
 HOMEPAGE="https://kristaps.bsd.lv/kcgi/"
@@ -89,8 +89,8 @@ src_test() {
 
 src_install() {
 	bmake DESTDIR="${D}" \
-	    DATADIR="${EPREFIX}/usr/share/doc/${PF}/examples" \
-	    install || die
+		DATADIR="${EPREFIX}/usr/share/doc/${PF}/examples" \
+		install || die
 
 	einstalldocs
 }

net-libs/ulfius/ulfius-2.7.15.ebuild
@@ -21,8 +21,8 @@ BDEPEND="
 DEPEND="
 	curl? ( net-misc/curl )
 	doc? (
-	     app-text/doxygen
-	     media-gfx/graphviz
+		app-text/doxygen
+		media-gfx/graphviz
 	)
 	jansson? ( dev-libs/jansson:= )
 	ssl? ( net-libs/gnutls:= )

net-p2p/monero/files/monerod-0.18.4.0.initd
@@ -16,13 +16,13 @@ retry="SIGTERM/30/SIGKILL/5"
 
 
 depend() {
-    after net
-    need localmount
+	after net
+	need localmount
 }
 
 start_pre() {
-    checkpath --directory --owner ${command_user} --mode 0755 \
-	      /var/lib/monero /var/log/monero $(dirname ${pidfile})
+	checkpath --directory --owner ${command_user} --mode 0755 \
+		/var/lib/monero /var/log/monero $(dirname ${pidfile})
 }
 
 status() {

net-voip/baresip/baresip-4.2.0.ebuild
@@ -13,9 +13,11 @@ LICENSE="BSD"
 SLOT="0"
 KEYWORDS="~amd64"
 
-IUSE="aac acip alsa amr aptx aom codec2 console dbus echo evdev ffmpeg gstreamer
-	  gtk3 jack httpd httpreq mpa mqtt opus pipewire portaudio pulseaudio sdl
-	  serreg snapshot sndfile spandsp static-libs syslog tcpcontrol v4l video vpx webrtc X"
+IUSE="
+	aac acip alsa amr aptx aom codec2 console dbus echo evdev ffmpeg gstreamer
+	gtk3 jack httpd httpreq mpa mqtt opus pipewire portaudio pulseaudio sdl
+	serreg snapshot sndfile spandsp static-libs syslog tcpcontrol v4l video vpx webrtc X
+"
 
 RE_PVF="$(ver_cut 1-2)*"
 

net-voip/baresip/baresip-4.3.0.ebuild
@@ -13,9 +13,11 @@ LICENSE="BSD"
 SLOT="0"
 KEYWORDS="~amd64"
 
-IUSE="aac acip alsa amr aptx aom codec2 console dbus echo evdev ffmpeg gstreamer
-	  gtk3 jack httpd httpreq mpa mqtt opus pipewire portaudio pulseaudio sdl
-	  serreg snapshot sndfile spandsp static-libs syslog tcpcontrol v4l video vpx webrtc X"
+IUSE="
+	aac acip alsa amr aptx aom codec2 console dbus echo evdev ffmpeg gstreamer
+	gtk3 jack httpd httpreq mpa mqtt opus pipewire portaudio pulseaudio sdl
+	serreg snapshot sndfile spandsp static-libs syslog tcpcontrol v4l video vpx webrtc X
+"
 
 RE_PVF="$(ver_cut 1-2)*"
 

net-voip/baresip/baresip-4.4.0.ebuild
@@ -13,9 +13,11 @@ LICENSE="BSD"
 SLOT="0"
 KEYWORDS="~amd64"
 
-IUSE="aac acip alsa amr aptx aom codec2 console dbus echo evdev ffmpeg gstreamer
-	  gtk3 jack httpd httpreq mpa mqtt opus pipewire portaudio pulseaudio sdl
-	  serreg snapshot sndfile spandsp static-libs syslog tcpcontrol v4l video vpx webrtc X"
+IUSE="
+	aac acip alsa amr aptx aom codec2 console dbus echo evdev ffmpeg gstreamer
+	gtk3 jack httpd httpreq mpa mqtt opus pipewire portaudio pulseaudio sdl
+	serreg snapshot sndfile spandsp static-libs syslog tcpcontrol v4l video vpx webrtc X
+"
 
 RE_PVF="$(ver_cut 1-2)*"
 

x11-wm/dk/dk-1.7.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2021-2022 Gentoo Authors
+# Copyright 2021-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -16,7 +16,7 @@ SLOT="0"
 KEYWORDS="~amd64 ~riscv ~x86"
 
 IUSE="examples man"
-RESTRICT=strip
+RESTRICT="strip"
 
 DEPEND="
 	x11-base/xcb-proto
@@ -52,7 +52,7 @@ src_install() {
 	cd natemaia-dk-bc9bd6349321 || die
 	if use man; then
 		sed "s/VERSION/${VERSION}/g" man/dk.1 || die
-	    doman man/*.*
+		doman man/*.*
 	fi
 
 	dobin dk dkcmd

x11-wm/dk/dk-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2021-2022 Gentoo Authors
+# Copyright 2021-2025 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=8
@@ -55,7 +55,7 @@ src_install() {
 	dodoc README.md
 	if use man; then
 		#sed "s/VERSION/${VERSION}/g" man/dk.1 || die
-	    doman man/*.*
+		doman man/*.*
 	fi
 	#dobin dk dkcmd
 	emake DESTDIR="${D}" PREFIX="${EPREFIX}/usr" DOC="/usr/share/doc/${PF}" MAN="/usr/share/man/man1/" install